f9d2a848750960dc7494c2fee145fd32a7a93e7e
Patrick Dufour <Patrick@gmail.com>
2024-04-08 17:41:24 +0200

    backend guimet ready

--- dashart-backend.js
+++ dashart-backend.js
@@ -39,15 +39,19 @@ router.get("/", (ctx) => {
     ctx.body = [
         "Usable routes :",
         "",
-        'GET <a href="http://localhost:3000/image-links">/image-links</a>',
-        'POST <a href="http://localhost:3000/increment-counter">/save-click</a>',
-        'GET <a href="http://localhost:3000/click-counter">/click-counter</a>',
-        'GET <a href="http://localhost:3000/last-requests">/last-requests</a>',
+        'GET <a href="http://localhost:3000/image-links-louvres">/image-links-louvres</a>',
+        'GET <a href="http://localhost:3000/image-links-guimet">/image-links-guimet</a>',
+        'POST <a href="http://localhost:3000/increment-counter-louvres">/save-click-louvres</a>',
+        'POST <a href="http://localhost:3000/increment-counter-guimet">/save-click-guimet</a>',
+        'GET <a href="http://localhost:3000/click-counter-louvres">/click-counter-louvres</a>',
+        'GET <a href="http://localhost:3000/click-counter-guimet">/click-counter-guimet</a>',
+        'GET <a href="http://localhost:3000/last-requests-louvres">/last-requests-louvres</a>',
+        'GET <a href="http://localhost:3000/last-requests-guimet">/last-requests-guimet</a>',
 
     ].join("<br>");
 });
 
-router.get("/image-links", async (ctx) => {
+router.get("/image-links-louvres", async (ctx) => {
     try {
         const imageLinksData = await fs.readJSON("datasets/ImageLinksLouvres.json");
         ctx.body = imageLinksData;
@@ -56,15 +60,24 @@ router.get("/image-links", async (ctx) => {
         ctx.body = { error: "Failed to read ImageLinksLouvres.json" };
     }
 });
+router.get("/image-links-guimet", async (ctx) => {
+    try {
+        const imageLinksData = await fs.readJSON("datasets/ImageLinksGuimet.json");
+        ctx.body = imageLinksData;
+    } catch (error) {
+        ctx.status = 500;
+        ctx.body = { error: "Failed to read ImageLinksGuimet.json" };
+    }
+});
 
-router.post("/save-click", async (ctx) => {
+router.post("/save-click-louvres", async (ctx) => {
     try {
         console.log(ctx.request.body);
         const { nom, timestamp } = ctx.request.body;
 
 
         // Read existing data from JSON file
-        let jsonData = await fs.readFile("data/clickCounts.json");
+        let jsonData = await fs.readFile("data/clickCountsLouvres.json");
         let clickCounts = JSON.parse(jsonData);
 
         // Check if nom exists in clickCounts
@@ -76,7 +89,7 @@ router.post("/save-click", async (ctx) => {
         clickCounts[nom].push(timestamp);
 
         // Write updated data back to JSON file
-        await fs.writeFile("data/clickCounts.json", JSON.stringify(clickCounts));
+        await fs.writeFile("data/clickCountsLouvres.json", JSON.stringify(clickCounts));
 
         ctx.body = { status: "success", message: "Click data saved successfully" };
     } catch (error) {
@@ -86,9 +99,38 @@ router.post("/save-click", async (ctx) => {
     }
 });
 
-router.get("/click-counter", async (ctx) => {
+router.post("/save-click-guimet", async (ctx) => {
     try {
-        const clickCountsData = await fs.readJSON("data/clickCounts.json");
+        console.log(ctx.request.body);
+        const { nom, timestamp } = ctx.request.body;
+
+
+        // Read existing data from JSON file
+        let jsonData = await fs.readFile("data/clickCountsGuimet.json");
+        let clickCounts = JSON.parse(jsonData);
+
+        // Check if nom exists in clickCounts
+        if (!clickCounts.hasOwnProperty(nom)) {
+            clickCounts[nom] = []; // Create an empty array if nom doesn't exist
+        }
+
+        // Add timestamp to the list associated with nom
+        clickCounts[nom].push(timestamp);
+
+        // Write updated data back to JSON file
+        await fs.writeFile("data/clickCountsGuimet.json", JSON.stringify(clickCounts));
+
+        ctx.body = { status: "success", message: "Click data saved successfully" };
+    } catch (error) {
+        console.error("Error saving click data:", error);
+        ctx.status = 500;
+        ctx.body = { error: "Failed to save click data" };
+    }
+});
+
+router.get("/click-counter-louvres", async (ctx) => {
+    try {
+        const clickCountsData = await fs.readJSON("data/clickCountsLouvres.json");
         let sortedList = [];
         for (const oeuvres in clickCountsData) {
             // Calculate the number of views (length of timestamps array)
@@ -104,20 +146,53 @@ router.get("/click-counter", async (ctx) => {
         ctx.body = sortedList;
     } catch (error) {
         ctx.status = 500;
-        ctx.body = { error: "Failed to read clickCounts.json" };
+        ctx.body = { error: "Failed to read clickCountsLouvres.json" };
+    }
+});
+router.get("/click-counter-guimet", async (ctx) => {
+    try {
+        const clickCountsData = await fs.readJSON("data/clickCountsGuimet.json");
+        let sortedList = [];
+        for (const oeuvres in clickCountsData) {
+            // Calculate the number of views (length of timestamps array)
+            const nombresdevue = clickCountsData[oeuvres].length;
+
+            // Push an object containing art name and number of views to the sorted list
+            sortedList.push({ oeuvres, nombresdevue });
+        }
+
+        // Sort the list based on the number of views in descending order
+        sortedList.sort((a, b) => b.nombresdevue - a.nombresdevue);
+
+        ctx.body = sortedList;
+    } catch (error) {
+        ctx.status = 500;
+        ctx.body = { error: "Failed to read clickCountsGuimet.json" };
     }
 });
 
-router.get("/last-requests", async (ctx) => {
+router.get("/last-requests-louvres", async (ctx) => {
     try {
-        const lastRequestData = await fs.readJSON("data/clickCounts.json");
+        const lastRequestData = await fs.readJSON("data/clickCountsLouvres.json");
         const lastFiveRequests = getLastFiveRequests(lastRequestData);
         ctx.body = lastFiveRequests;
     } catch (error) {
         ctx.status = 500;
-        ctx.body = { error: "Failed to read clickCounts.json" };
+        ctx.body = { error: "Failed to read clickCountsLouvres.json" };
     }
 });
+
+router.get("/last-requests-guimet", async (ctx) => {
+    try {
+        const lastRequestData = await fs.readJSON("data/clickCountsGuimet.json");
+        const lastFiveRequests = getLastFiveRequests(lastRequestData);
+        ctx.body = lastFiveRequests;
+    } catch (error) {
+        ctx.status = 500;
+        ctx.body = { error: "Failed to read clickCountsGuimet.json" };
+    }
+});
+
 
 app.use(bodyParser());
 app.use(router.routes()).use(router.allowedMethods());
